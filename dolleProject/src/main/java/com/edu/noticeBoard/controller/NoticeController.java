5d7e18001e0bae17ba7feababc992c6f1c6521c4
TJ <TJ@DESKTOP-J2LI030>
2020-07-07 16:13:08 +0900

    [작성자]이용훈 [작성시간]2020-07-07 16:12
    [맡은역할]공지 게시판
    [작업한내용]공지 게시판 추가 기능
    [진행중내용]공지 게시판 추가 기능
    [앞으로할것]공지 게시판 삭제 기능

--- dolleProject/src/main/java/com/edu/noticeBoard/controller/NoticeController.java
+++ dolleProject/src/main/java/com/edu/noticeBoard/controller/NoticeController.java
@@ -3,6 +3,8 @@ package com.edu.noticeBoard.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.edu.member.vo.MemberVo;
 import com.edu.noticeBoard.service.NoticeService;
 import com.edu.noticeBoard.vo.NoticeMemberFileVo;
 
@@ -24,13 +29,6 @@ public class NoticeController {
 	private NoticeService noticeService;
 	
 	
-	@RequestMapping(value="/noticeBoard/adminList.do", method = RequestMethod.GET)
-	public String adminNoticeBoardListView(Model model) {
-		
-		
-		
-		return "noticeBoard/adminNoticeBoardListView";
-	}
 	
 	@RequestMapping(value="/noticeBoard/detail.do", method = RequestMethod.GET)
 	public String noticeBoardDetailView(Model model
@@ -90,12 +88,73 @@ public class NoticeController {
 			}
 		}
 		
-		model.addAttribute("noticeMemberFileList",noticeMemberFileList);
+		model.addAttribute("noticeMemberFileList", noticeMemberFileList);
 		model.addAttribute("noticeMemberFileFixedList", tempList);
 		
 		
 		
 		return "noticeBoard/noticeBoardListView";
 	}
+	
+	@RequestMapping(value = "/noticeBoard/adminList.do", method = { RequestMethod.GET, RequestMethod.POST })
+	public String adminNoticeBoardListView(Model model) {
+
+		log.debug(" *** Welcome adminNoticeBoardListView {}***");
+
+		List<NoticeMemberFileVo> noticeMemberFileList = noticeService.noticeMemberFileList();
+		List<NoticeMemberFileVo> noticeMemberFileFixedList = noticeService.noticeMemberFileFixedList();
+
+// tempList에 고정으로 띄울 최신글 4개를 저장하고 tempList만 보내서 따로 거르는 과정
+		int fixedListCount = noticeMemberFileFixedList.size();
+		int limitNum = 4;
+		List<NoticeMemberFileVo> tempList = new ArrayList<NoticeMemberFileVo>();
+		if (fixedListCount > limitNum) {
+			for (int i = 0; i < limitNum; i++) {
+				tempList.add(noticeMemberFileFixedList.get(i));
+			}
+		} else {
+			for (int i = 0; i < noticeMemberFileFixedList.size(); i++) {
+				tempList.add(noticeMemberFileFixedList.get(i));
+			}
+		}
+
+		model.addAttribute("noticeMemberFileList", noticeMemberFileList);
+		model.addAttribute("noticeMemberFileFixedList", tempList);
+
+		return "noticeBoard/adminNoticeBoardListView";
+	}
+	
+	@RequestMapping(value = "/noticeBoard/AdminNoticeAdd.do", method = {RequestMethod.GET})
+	public String adminNoticeAdd(HttpSession session, Model model) {
+		
+		log.info("call AdminNoticeAdd!");
+		
+		MemberVo sessionMemberVo = 
+				(MemberVo)session.getAttribute("_memberVo_");
+		String noticeNickname = sessionMemberVo.getNickname();
+		
+//		model.addAttribute(noticeNickname);
+		
+		log.info("------------------------------");
+		log.info(sessionMemberVo.toString());
+		log.info("------------------------------");
+		
+		return "/noticeBoard/adminNoticeBoardAddView";
+	}
+	
+	@RequestMapping(value = "/noticeBoard/AdminNoticeAddCtr.do", method = {RequestMethod.GET, RequestMethod.POST})
+	public String adminNoticeAdd(Model model
+			, NoticeMemberFileVo noticeMemberFileVo
+			, @RequestParam(defaultValue = "none") String fixed) {
+		log.info("에러찾기 쉽지만 쓰기는 귀찮다");
+		
+		noticeMemberFileVo.setNoticeFixed(fixed);
+		
+		noticeService.noticeInsertOne(noticeMemberFileVo);
+		
+		
+		return "redirect:/noticeBoard/AdminList.do";
+	}
+	
 	
 }
